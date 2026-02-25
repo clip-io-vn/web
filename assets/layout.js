884d673dc42a62306cbf0386b5820f190f35e64a
clip-io-vn <hoangtuananh111987@gmail.com>
2026-02-25 13:12:43 +0700

    Create layout.js

---
+++ assets/layout.js
@@ -0,0 +1,12 @@
+function loadHTML(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadHTML("header", "/header.html");
+    loadHTML("footer", "/footer.html");
+});
